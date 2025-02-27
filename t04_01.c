d4321c96a893fce3b410d6e6622cf905154c56d2
DeaHutapea <deahutapea99@gmail.com>
2025-02-27 16:19:03 +0700

    pair 04 sudah siap melakukan praktikum

--- t04_01.c
+++ t04_01.c
@@ -1,10 +1,33 @@
-// NIM - Name
-// NIM - Name
+// 12S24053 - Dea Hutapea
+// 12S24058 - Airene Situmorang
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-int main(int _argv, char **_argc) {
-  // codes
+int main(int argc, char **argv) {
+    char input[61]; 
+    printf("Input: ");
+    scanf("%60s", input);
 
-  return 0;
+    int length = strlen(input);
+    if (length < 3 || length > 60 || length % 3 != 0) {
+        printf("Input tidak valid.\n");
+       return 1;
+    }
+
+    printf("Output: ");
+    for (int i = 0; i < length; i += 3) {
+        char Ascii_str[4];
+        Ascii_str[0] = input[i];
+        Ascii_str[1] = input[i+1];
+        Ascii_str[2] = input[i+2];
+        Ascii_str[3] = '\0';
+
+        int Ascii_code = atoi(Ascii_str);
+        printf("%c", (char)Ascii_code);
+    }
+    printf("\n");
+   
+    return 0;
 }
--- t04_02.c
+++ t04_02.c
@@ -1,10 +1,20 @@
-// NIM - Name
-// NIM - Name
+// 12S24053 - Dea Hutapea
+// 12S24058 - Airene Situmorang
 
 #include <stdio.h>
+#include <string.h>
 
-int main(int _argv, char **_argc) {
-  // codes
-
-  return 0;
+int main(int argc, char **argv) {
+    char string[21];
+    printf("Input: ");
+    scanf("%20[^\n]", string);  
+    
+    int length = strlen(string);
+    printf("Output: ");
+    for (int i = 0; i < length; i++) {
+        printf("%03d", string[i]); 
+    }
+    printf("013\n");  
+    
+    return 0;
 }
